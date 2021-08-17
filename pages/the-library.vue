8d7dc6b7409ba0c1f87866eb468e90153d024e1f
Kyle Milloy <k@fanda.io>
2021-08-17 11:53:09 -0600

    add notes to room description

--- pages/the-library.vue
+++ pages/the-library.vue
@@ -3,10 +3,21 @@
     <div>
       <p class="text-center mb-4">Exit</p>
       <div class="grid grid-cols-3 grid-rows-3 gap-4 mb-4 justify-between">
-        <button
+        <!--
+          Make this a div instead of a button so that pressing "space"
+          doesn't trigger a "click" event
+        -->
+        <div
           v-for="i in max"
           :key="i"
-          class="text-white flex items-center justify-center h-24"
+          class="
+            text-white text-center
+            cursor-pointer
+            flex
+            items-center
+            justify-center
+            h-24
+          "
           :class="location == section(i) ? 'bg-red-500' : 'bg-indigo-500'"
           @click="setRoom(i)"
         >
@@ -17,9 +28,11 @@
               type="text"
               class="min-w-0 w-16 mx-6 my-2 text-gray-600"
               @click.stop
+              @keyup.stop
+              @keydown.stop
             />
           </div>
-        </button>
+        </div>
       </div>
       <p class="text-center mt-4 mb-12">Entrance</p>
 
@@ -36,9 +49,9 @@
           mb-4
         "
         type="button"
-        @click="roll"
+        @click="shift"
       >
-        Roll
+        Shift
       </button>
 
       <button
@@ -56,10 +69,15 @@
         <span class="block font-bold" v-text="room.name.trim()" />
         <p class="block whitespace-pre-line" v-text="room.description.trim()" />
 
-        <span v-if="room.challenge" class="block font-bold mt-4">
-          Challenge
-        </span>
-        <p v-if="room.challenge" v-text="room.challenge.trim()" />
+        <div v-if="room.challenge" class="mt-4">
+          <span class="block font-bold">Challenge</span>
+          <p v-text="room.challenge.trim()" />
+        </div>
+
+        <div v-if="marked[location]" class="mt-4">
+          <span class="block font-bold">Notes</span>
+          <p v-text="marked[location]" />
+        </div>
       </div>
 
       <div class="absolute top-4 right-4 text-right">
@@ -99,7 +117,7 @@ export default {
       offset: 0,
       max: 9,
       marked: new Array(this.max).map(() => false),
-      rolls: [],
+      shifts: [],
       location: 0,
       mutations: [0, 1, 3, 5, 7, 2, 4, 6, 8],
       mutation: 0,
@@ -117,25 +135,25 @@ export default {
     },
   },
   methods: {
-    roll() {
+    shift() {
       this.mutation += 1
 
       if (this.mutation >= this.mutations.length) {
         this.mutation = 0
       }
 
-      const roll = this.mutations[this.mutation]
-      this.rolls.push(roll)
-      this.offset = roll
+      const shift = this.mutations[this.mutation]
+      this.shifts.push(shift)
+      this.offset = shift
     },
 
     undo() {
-      if (this.rolls.length === 0) {
+      if (this.shifts.length === 0) {
         return
       }
 
-      this.rolls.pop()
-      let offset = this.rolls[this.rolls.length - 1]
+      this.shifts.pop()
+      let offset = this.shifts[this.shifts.length - 1]
       let mutation = this.mutation - 1
 
       if (mutation < 0) {
